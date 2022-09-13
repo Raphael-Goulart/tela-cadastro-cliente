75552a2be7298a5b97a981939ea8fbfb760c06b5
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-09-13 16:11:42 +0100

    starting a customer registration project

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('mandando oi para amigo')
+
+function mandaMensagem () {
+    console.log('oi');
+    console.log('oi, tudo bem?');
+    console.log('mensagem recebida');
+}
+
+setTimeout(mandaMensagem, 5000);
+
+console.log('tchau');
