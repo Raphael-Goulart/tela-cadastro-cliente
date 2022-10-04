52e8972a54f43d47ff7041d04732caf27f58924a
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-10-04 19:28:23 +0100

    remove try and catch

--- main.js
+++ main.js
@@ -14,19 +14,6 @@ console.log(consultaCEP);
 */
 
 async function buscaEndereco(cep) {
-    try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        
-        if (consultaCEPConvertida.erro){
-            throw Error('CEP não Existe');
-        }
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
-    } catch (erro) {
-        console.log(erro)
-    }
- 
 }
 
 
