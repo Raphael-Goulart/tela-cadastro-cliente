fd578098025b05d1d9178a63ff320df9942049f2
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-10-10 17:50:30 +0100

    added return to async function

--- main.js
+++ main.js
@@ -14,7 +14,27 @@ console.log(consultaCEP);
 */
 
 async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+    
+        if (consultaCEPConvertida.erro) {
+             throw Error('CEP não Existe');
+        }
+        console.log(consultaCEPConvertida); 
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro)
+
+    }
+
 }
+
+let ceps = ['01001000', '01001001'];
+let conjutoCeps = ceps.map(valores => buscaEndereco(valores));
+buscaEndereco();
+
+
 
 
 
